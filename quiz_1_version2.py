67d15f5eb1bffb29250a6255fbbdb97cc34d8499
Yingna <48152350+tiancaiji@users.noreply.github.com>
2019-06-06 23:48:21 +1000

    quiz 1, version 2

--- quiz_1_version2.py
+++ quiz_1_version2.py
@@ -21,16 +21,31 @@ print('\nThe generated mapping is:')
 print('  ', mapping)
 
 one_to_one_part_of_mapping = {}
+mapping_as_a_list = []
+nonkeys = []
 
-nonkeys = [i for i in range(1, upper_bound) if i not in mapping]
-mapping_as_a_list = [None if i not in mapping else mapping[i] for i in range(0, upper_bound)]
+for i in range(0, upper_bound):
+    if i not in mapping:
+        # 知识点：list.append()方法
+        mapping_as_a_list.append(None)
+        if i != 0:
+            nonkeys.append(i)
+    else:
+        mapping_as_a_list.append(mapping[i])
+
+# 计数器，记录每个value出现几次
 counter_dict = {}
 for item in mapping.items():
     counter_dict[item[1]] = counter_dict.get(item[1], 0) + 1
+
+# 遍历mapping，选出value出现一次的key
+# 知识点：dict.items()方法
 for item in mapping.items():
     if counter_dict[item[1]] == 1:
         one_to_one_part_of_mapping[item[0]] = item[1]
 print()
+# google搜索"f-string"
+# 知识点：内建len()函数
 print(f'The mappings\'s so-called "keys" make up a set whose number of elements is {len(mapping)}.')
 print('\nThe list of integers between 1 and', upper_bound - 1, 'that are not keys of the mapping is:')
 print('  ', nonkeys)
